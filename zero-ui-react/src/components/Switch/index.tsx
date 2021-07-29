c71c36aa9285e85d6e02026c10f73680ba071d1a
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-07-29 20:52:36 +0800

    修改hooks目录结构

--- zero-ui-react/src/components/Switch/index.tsx
+++ zero-ui-react/src/components/Switch/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import classNames from "classnames";
-import myUseEffect from '../hooks/myUseEffect';
+import myUseEffect from '../../hooks/myUseEffect';
 import './index.scss';
 
 interface SwitchProps {
