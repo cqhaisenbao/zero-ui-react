1accc7ae8797100233f06ad40183553f6f11e6b0
cqhaisenbao <fanyu9353@gmail.com>
2022-02-26 21:39:05 +0800

    test: input 单元测试

---
+++ src/components/Input/input.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Input, { InputProps } from './index';
+import '@testing-library/jest-dom/extend-expect';
+
+const defaultProps: InputProps = {
+  onChange: jest.fn(),
+  placeholder: 'test-input',
+};
+describe('test Input component', () => {
+  it('should render the correct default Input', () => {
+    const { getByPlaceholderText } = render(<Input {...defaultProps} />);
+    const testNode = getByPlaceholderText('test-input') as HTMLInputElement;
+    expect(testNode).toBeInTheDocument();
+    expect(testNode).toHaveClass('zero-input-inner');
+    fireEvent.change(testNode, { target: { value: '11' } });
+    expect(defaultProps.onChange).toHaveBeenCalled();
+    expect(testNode.value).toEqual('11');
+  });
+  it('should render the disabled Input on disabled property', () => {
+    const { getByPlaceholderText } = render(
+      <Input disabled placeholder="disabled" />,
+    );
+    const testNode = getByPlaceholderText('disabled') as HTMLInputElement;
+    expect(testNode.disabled).toBeTruthy();
+  });
+  it('should render different input sizes on size property', () => {
+    const { container } = render(<Input placeholder="sizes" size="lg" />);
+    const testContainer = container.querySelector('.zero-input-wrapper');
+    expect(testContainer).toHaveClass('input-size-lg');
+  });
+  it('should render prepand and append element on prepand/append property', () => {
+    const { queryByText, container } = render(
+      <Input placeholder="pend" prepand="https://" append=".com" />,
+    );
+    const testContainer = container.querySelector('.zero-input-wrapper');
+    expect(testContainer).toHaveClass(
+      'input-group input-group-append input-group-prepend',
+    );
+    expect(queryByText('https://')).toBeInTheDocument();
+    expect(queryByText('.com')).toBeInTheDocument();
+  });
+});
